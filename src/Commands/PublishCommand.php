18d939d6255cdc028dcc8dfab7226f9a10b37b4e
Matt Crowe <github.mpc@zym.me>
2019-02-12 17:33:06 +0000

    continue unified documentation

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -16,7 +16,7 @@ class PublishCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'belt-workflow:publish {action=publish} {--force} {--include=} {--exclude=} {--config}';
+    protected $signature = 'belt-workflow:publish {action=publish} {--include=} {--exclude=} {--force} {--config} {--prune}';
 
     /**
      * The console command description.
